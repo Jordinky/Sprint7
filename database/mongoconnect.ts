aaf6c59dfe19e6fa6e3767fa9ebe35a7408946e6
Jordi <jordipelaez05@gmail.com>
2024-10-02 13:23:24 +0200

    arreglado mongo_URL

---
+++ database/mongoconnect.ts
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv"
+
+dotenv.config()
+const url = process.env.MONGO_URI ?? ''
+
+const connect = async () =>{
+  try{
+    await mongoose.connect(url)
+    console.log("connected to mongoDB")
+  }catch(error:any){
+    console.log("error on connecting to mongoDB" + error.message)
+  }
+}
+export default connect 
